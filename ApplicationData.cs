5fa7252773f1f937b60d5fe15ccaadaf9d227c87
kaplego <70806521+kaplego@users.noreply.github.com>
2024-05-28 08:28:36 +0200

    Maquette terminée

--- ApplicationData.cs
+++ ApplicationData.cs
@@ -5,18 +5,18 @@ namespace MaquetteBotanic
 {
     public class ApplicationData
     {
-        private List<Produit> templateProduits;
+        private List<ProduitAchat> templateProduits;
 
-        public List<Produit> TemplateProduits { get => this.templateProduits; set => this.templateProduits = value; }
+        public List<ProduitAchat> TemplateProduits { get => this.templateProduits; set => this.templateProduits = value; }
 
         public ApplicationData()
         {
-            this.templateProduits = new List<Produit>()
+            this.templateProduits = ProduitAchat.FromListProduit(new List<Produit>()
             {
                 new Produit("Produit 1", 19.99, "Description du produit 1", CouleurProduit.Blanc, TailleProduit.Petit),
                 new Produit("Produit 2", 9.99, "Description du produit 2", CouleurProduit.Rouge, TailleProduit.TresPetit),
                 new Produit("Produit 3", 149.99, "Description du produit 3", CouleurProduit.Noir, TailleProduit.Grand),
-            };
+            });
         }
     }
 }
---
+++ ProduitAchat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace MaquetteBotanic
+{
+    public class ProduitAchat : Produit
+    {
+        private int quantite;
+
+        public int Quantite { get => this.quantite; set => this.quantite = value; }
+
+        public ProduitAchat(string nom, double prix, string description, CouleurProduit couleur, TailleProduit taille)
+            : base(nom, prix, description, couleur, taille)
+        {
+            this.Quantite = 0;
+        }
+
+        public ProduitAchat(Produit produit)
+            : this(produit.Nom, produit.Prix, produit.Description, produit.Couleur, produit.Taille)
+        { }
+
+        public static List<ProduitAchat> FromListProduit(List<Produit> liste)
+        {
+            List<ProduitAchat> listeAchat = new List<ProduitAchat>();
+            foreach (Produit produit in liste)
+            {
+                listeAchat.Add(new ProduitAchat(produit));
+            }
+            return listeAchat;
+        }
+
+        public double PrixTotal()
+        {
+            return this.Quantite * this.Prix;
+        }
+    }
+}
