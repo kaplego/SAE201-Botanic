f1017ff1c601e6200cae92e13b2c13403b119579
Jordan Lebret <jordan.lebret@etu.univ-smb.fr>
2024-06-03 16:30:30 +0200

    Création classe magasin

---
+++ Magasin.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquetteBotanic
+{
+    internal class Magasin
+    {
+
+        private int id;
+        private string nom;
+        private string adresseRue;
+        private string adresseCP;
+        private string adresseVille;
+        private string horaire;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string AdresseRue
+        {
+            get
+            {
+                return adresseRue;
+            }
+
+            set
+            {
+                adresseRue = value;
+            }
+        }
+
+        public string AdresseCP
+        {
+            get
+            {
+                return adresseCP;
+            }
+
+            set
+            {
+                adresseCP = value;
+            }
+        }
+
+        public string AdresseVille
+        {
+            get
+            {
+                return adresseVille;
+            }
+
+            set
+            {
+                adresseVille = value;
+            }
+        }
+
+        public string Horaire
+        {
+            get
+            {
+                return this.horaire;
+            }
+
+            set
+            {
+                this.horaire = value;
+            }
+        }
+    }
+}
