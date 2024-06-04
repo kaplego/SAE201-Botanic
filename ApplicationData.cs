ce7f1f0a6179f1a661db1eebcf8ca4757618e2e6
kaplego <70806521+kaplego@users.noreply.github.com>
2024-06-04 15:51:45 +0200

    Navigabilité

--- ApplicationData.cs
+++ ApplicationData.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Automation.Peers;
 using static MaquetteBotanic.Produit;
 
 namespace MaquetteBotanic
 {
     public class ApplicationData
     {
+        private static ApplicationData instance;
+        private ObservableCollection<TypeProduit> typesProduit;
+        private ObservableCollection<Categorie> categories;
         private ObservableCollection<ProduitAchat> produits;
 
+        public static ApplicationData Instance {
+            get {
+                instance ??= new ApplicationData();
+                return instance;
+            }
+        }
+
+        public ObservableCollection<TypeProduit> TypesProduit { get => this.typesProduit; set => this.typesProduit = value; }
+        public ObservableCollection<Categorie> Categories { get => categories; set => categories = value; }
         public ObservableCollection<ProduitAchat> Produits { get => this.produits; set => this.produits = value; }
 
-        public ApplicationData()
+        private ApplicationData()
         {
-            this.Produits = ProduitAchat.FromListProduit(Produit.Read());
+            TypesProduit = TypeProduit.Read();
+            Categories = Categorie.Read();
+            Produits = ProduitAchat.FromListProduit(Produit.Read());
+        }
+
+        public delegate bool CallBack<T>(T item);
+
+        public static T? Find<T>(ObservableCollection<T> from, CallBack<T> callback)
+        {
+            T? result = default;
+            foreach (T item in from)
+            {
+                if (callback.Invoke(item))
+                {
+                    result = item;
+                }
+            }
+            return result;
         }
     }
 }
